703c9078646dde9ba94fc7b1a37f7ecb0d142e85
CpE Chair <kmlcburce@gmail.com>
2021-03-16 22:07:39 +0800

    added scrambler function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <string.h>
 using namespace std;
 class GridCipher{
 private:
@@ -44,67 +45,157 @@ public:
     }
 
     //methods
-    void display(string grid[6][6]){ //displays grid in 6x6 matrix form (might proc an error. Low priority in fix queue, it's all for display)
+    void display(string grid[6][6]){ //displays grid in 6x6 matrix form (Error here. Fix priority: Low)
         int i,j,ctr=0;
-        for(i=0;i<=6;++i){
-            for(j=0;j<=6;++j){
-                if(ctr<6){
-                    cout << grid[i][j] << " ";
-                    ctr++;
-                }else{
-                    cout << endl;
-                    ctr = 0;
+            for(i=0;i<=6;i++){
+                for(j=0;j<=6;j++){
+                    if(ctr<6){
+                        cout << grid[i][j] << " ";
+                        ctr++;
+                    }else{
+                        cout << endl;
+                        ctr = 0;
+                    }
+
                 }
-
             }
-        }
-
+            cout << "Error occurs here";
     }
     //initialize Grid
     void initGrid(){
-    string grid[6][6];
-    int i,j,x=0,ctr=48, letterCtr=0;
-    /*string *cursor;*/
-    char letter;
-    char letterList[26];
-        for(letter = 'A'; letter <= 'Z'; ++letter){
-            letterList[x] = letter;
-            x++;
+        string grid[6][6];
+        int i,j,x=0,ctr=48, letterCtr=0;
+        /*string *cursor;*/
+        char letter;
+        char letterList[26];
+            for(letter = 'A'; letter <= 'Z'; letter++){
+                letterList[x] = letter;
+                x++;
 
-         }
+             }
 
-            for(i=0;i<6;++i){
-                for(j=0;j<6;++j){
+                for(i=0;i<6;i++){
+                    for(j=0;j<6;j++){
 
-                    if(i<=1 && j<6){ //checks if it doesnt occupy space where numbers are supossed to be
-                            if(ctr<58){ //ASCII numbers starts from 48
-                                    grid[i][j] = ctr++;
-                                    /*if(j+1<=6){//test condition: pointer *cursor points to the current element in the grid, may be used to point to next element
-                                        cursor = (*(grid+1)+j);
-                                        cout << *cursor << endl;
-                                    }else if(j+1 >6 && i<=6){
+                        if(i<=1 && j<6){ //checks if it doesnt occupy space where numbers are supossed to be
+                                if(ctr<58){ //ASCII numbers starts from 48
+                                        grid[i][j] = ctr++;
+                                        /*if(j+1<=6){//test condition: pointer *cursor points to the current element in the grid, may be used to point to next element
+                                            cursor = (*(grid+1)+j);
+                                            cout << *cursor << endl;
+                                        }else if(j+1 >6 && i<=6){
 
-                                    }*/
+                                        }*/
 
-                            }else{
+                                }else{
+                                grid[i][j] = letterList[letterCtr];
+                                letterCtr++;
+                                }
+
+                        }else{
                             grid[i][j] = letterList[letterCtr];
                             letterCtr++;
-                            }
+                        }
+                    }
 
-                    }else{
-                        grid[i][j] = letterList[letterCtr];
-                        letterCtr++;
+    getCoords(grid,"word");
+
+    }
+    //display(grid);
+    //vswap(grid,0,1); test out hswap and vswap here
+    }
+    //gets letter coordinates from grid and stores them in arr
+    void getCoords(string grid[6][6], string word){
+    int i, j,letterCtr=0, arrCtrX,arrCtrY, wordSize = word.size();
+    string letter[wordSize];
+    int arr[wordSize][2];
+
+        for(i=0;i<wordSize;i++){
+            letter[i] = word[i]; //isolate individual letters on input string
+        }
+        for(arrCtrX=0;arrCtrX<wordSize;arrCtrX++){//Loops for x value of arr[x][2]
+
+                    for(i=0;i<6;i++){//Loops through grid[][] to find matching letter
+                        for(j=0;j<6;j++){
+                            if(strcmp(grid[i][j], letter[letterCtr]) = 0){ //can string be == with char?
+                                cout << "Error occurs here";
+                                arr[arrCtrX][0] =  i;
+                                arr[arrCtrX][1] = j;
+                                letterCtr++;
+                        }
                     }
                 }
+
+        }
+        for(arrCtrX=0;arrCtrX<wordSize;arrCtrX++){ //print out arr to check
+            for(arrCtrY=0;arrCtrY<2;arrCtrY++){
+                cout << arr[arrCtrX][arrCtrY] << " ";
+            }
+        }
+
+
+    }
+    //creates new grid to be scrambled
+    void gridScrambler(){
+        string newGrid[6][6];
+        int i,j,x=0,ctr=48, letterCtr=0;
+        char letter;
+        char letterList[26];
+            for(letter = 'A'; letter <= 'Z'; ++letter){
+                letterList[x] = letter;
+                x++;
+
+             }
+
+                for(i=0;i<6;i++){
+                    for(j=0;j<6;j++){
+
+                        if(i<=1 && j<6){ //checks if it doesnt occupy space where numbers are supossed to be
+                                if(ctr<58){ //ASCII numbers starts from 48
+                                        grid[i][j] = ctr++;
+                                }else{
+                                grid[i][j] = letterList[letterCtr];
+                                letterCtr++;
+                                }
+
+                        }else{
+                            grid[i][j] = letterList[letterCtr];
+                            letterCtr++;
+                        }
+                    }
+            }
+    }
+
+
+    //gets new character based on original grid's coordinates
+    void gridCompare(string grid[6][6]){
 
 
 
     }
-    vswap(grid,0,1); //test out hswap and vswap here
+    //moves the grid to specified rotations through row-col swapping
+    void hswap(string grid[6][6], int x, int y){
+    int ctr;
+    string temp;
+        for(ctr=0;ctr<6;ctr++){
+            temp=grid[x][ctr];
+            grid[x][ctr]=grid[y][ctr];
+            grid[y][ctr]=temp;
 
-}
-    //algo 1: user input acts key to rotation (Ignore this Ridenzel)
-    void binaryConverter(string password){
+        }
+    display(grid);
+    }
+    void vswap(string grid[6][6], int x, int y){ // not implemented yet
+    int ctr;
+    string temp;
+    for(ctr=0;ctr<6;ctr++){
+        temp=grid[x][ctr];
+        grid[ctr][x]=grid[ctr][y];
+        grid[ctr][y]=temp;
+    }
+    display(grid);
+    }
+    /*void binaryConverter(string password){
         int i,remainder;
         char letter[password.size()];
         int ascii[password.size()], remainderSet[10];
@@ -124,34 +215,7 @@ public:
             }
         }
 
-    }
-    //moves the grid to specified rotations through shifting (Also ignore this)
-    void gridOperation(string grid[6][6], int code){
-
-
-
-    }
-    void hswap(string grid[6][6], int x, int y){ //problem function here
-    int ctr;
-    string temp;
-        for(ctr=0;ctr<6;ctr++){
-            temp=grid[x][ctr];
-            grid[x][ctr]=grid[y][ctr];
-            grid[y][ctr]=temp;
-
-        }
-    display(grid); // display grid
-    }
-    void vswap(string grid[6][6], int x, int y){ // not implemented yet
-    int ctr;
-    string temp;
-    for(ctr=0;ctr<6;ctr++){
-        temp=grid[x][ctr];
-        grid[ctr][x]=grid[ctr][y];
-        grid[ctr][y]=temp;
-    }
-    display(grid);
-}
+    }*/
 
 };
 
